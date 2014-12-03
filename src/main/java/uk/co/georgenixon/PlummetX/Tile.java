0ceebf0eccfc958b40c4ee31f4007429d4402a6c
George Nixon <g@georgenixon.co.uk>
2014-12-03 19:35:13 +0000

    Test Opaque turns to Partial

--- src/main/java/uk/co/georgenixon/PlummetX/Tile.java
+++ src/main/java/uk/co/georgenixon/PlummetX/Tile.java
@@ -4,24 +4,11 @@ package uk.co.georgenixon.PlummetX;
  * Created by g on 30/08/14.
  */
 public class Tile {
-    private final TileOpacity opacity;
+    private TileOpacity opacity;
     private Integer value;
 
-    void setValue(Integer value) {
-
-//        if (value == 0)
-//            throw new IllegalArgumentException("Zero is not a valid tile");
-//
-//        // We're using monospace single columns right now,
-//        // so reject numbers that won't fit the view!
-//        if (value < 0 || value > 9)
-//            throw new IllegalArgumentException("Number is too wide!");
-
-        this.value = value;
-    }
-
     public Tile(Integer value, TileOpacity opacity) {
-        setValue(value);
+        this.value = value;
         this.opacity = opacity;
     }
 
@@ -51,4 +38,11 @@ public class Tile {
         return value.toString().charAt(0);
     }
 
+    // Break apart or disappear depending on opacity, e.g.
+    // X -> x
+    // x -> 6
+    // 6 -> <empty>
+    public void destruct() {
+        this.opacity = TileOpacity.partial;
+    }
 }
